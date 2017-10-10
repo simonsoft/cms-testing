401dcbe6793f181fa0c67e8e2305512f875082ee
Thomas Åkesson <thomas.akesson@simonsoft.se>
2017-10-10 15:33:47 +0200

    License format

--- src/main/java/se/simonsoft/cms/testing/svn/CmsTestRepository.java
+++ src/main/java/se/simonsoft/cms/testing/svn/CmsTestRepository.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009-2013 Simonsoft Nordic AB
+ * Copyright (C) 2009-2017 Simonsoft Nordic AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
